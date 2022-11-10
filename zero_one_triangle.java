49be136ed2f00e95d8cd69abbe78c9369bcf5662
Raian-Siddique <raian0197@gmail.com>
2022-11-10 23:16:19 +0100

    final

--- zero_one_triangle.java
+++ zero_one_triangle.java
@@ -4,7 +4,6 @@
 public class zero_one_triangle {
 
     public static void zero_one_triangle(int n) {
-
         // outer Loop
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
