b112f7301e5bb0681fe7b935f6a4d04003df401b
Raian-Siddique <raian0197@gmail.com>
2022-11-10 23:09:29 +0100

    final

---
+++ zero_one_triangle.java
@@ -0,0 +1,24 @@
+/**
+ * zero_one_triangle
+ */
+public class zero_one_triangle {
+
+    public static void zero_one_triangle(int n) {
+
+        // outer Loop
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        zero_one_triangle(5);
+    }
+}
